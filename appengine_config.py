b281ded626445ea19f17d55069e772c22552e4c1
jeremydw <jeremydw@grow.io>
2013-11-21 21:53:50 -0800

    GCDC checkin.

--- appengine_config.py
+++ appengine_config.py
@@ -5,3 +5,12 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'growedit'))
 
 from grow import submodules
 submodules.fix_imports()
+
+from growedit.users import boto_auth  # Needed to register AuthHandler.
+
+def webapp_add_wsgi_middleware(app):
+  from google.appengine.ext.appstats import recording
+  app = recording.appstats_wsgi_middleware(app)
+  return app
+
+appstats_MAX_STACK = 20
--- utils/devappserver.sh
+++ utils/devappserver.sh
@@ -1,1 +1,1 @@
-dev_appserver.py --host=0.0.0.0 --port=8081 .
+dev_appserver.py --host=0.0.0.0 --port=8081 --allow_skipped_files=yes .
